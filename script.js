a8bed48420db5eebc326176b6c6cafe8c4fec66c
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-06-12 13:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // const num = 9;
 function fibonacci(num) {
 // your code here
-	 if (num <= 1)
-        return num;
-    return fibonacci(num-1) + fibonacci(num-2);
+	  let f = new Array(num+2); // 1 extra to handle case, n = 0
+        let i;
+        /* 0th and 1st number of the series are 0 and 1*/
+        f[0] = 0;
+        f[1] = 1;
+        for (i = 2; i <= num; i++)
+        {
+            /* Add the previous 2 numbers in the series
+            and store it */
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f[num];
 }
-// console.log(fibonacci(num));
+ console.log(fibonacci(num));
 
 module.exports = fibonacci;
