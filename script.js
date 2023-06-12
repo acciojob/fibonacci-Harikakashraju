f13a8f630aef0255fba445f5fa91c0a1722a784b
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-06-12 12:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function fibonacci(num) {
 // your code here
+	 if (num <= 1)
+        return num;
+    return fib(num-1) + fib(num-2);
 }
+console.log(fibonacci(num));
 
 module.exports = fibonacci;
